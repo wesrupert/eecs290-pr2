ca92d5c8bd5b96b0607b7b49866757a62dbe6d45
Blake <blake.needleman@gmail.com>
2013-02-19 15:58:23 -0500

    Added platforms

---
+++ Assets/Scripts/ElevatorPlatform.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using System.Collections;
+
+public class ElevatorPlatform : MonoBehaviour {
+
+	public Vector3 upperBound;
+	public Vector3 lowerBound;
+	private bool reachedUpper = false;
+	private bool reachedLower = false;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		
+	}
+	
+	void FixedUpdate() {
+		float HorizontalIncrement = (float).5*Time.fixedDeltaTime;
+		float VerticalIncrement = (float)1.5*Time.fixedDeltaTime;
+		
+		//left and right floating platform
+		if((upperBound.x - lowerBound.x) != 0) {
+			if(this.transform.position.x >= upperBound.x) {
+					reachedUpper = true;
+					reachedLower = false;
+			}
+			if(this.transform.position.x <= lowerBound.x) {
+					reachedLower = true;
+					reachedUpper = false;
+			}
+			if(reachedUpper == false) {
+				transform.Translate(HorizontalIncrement, 0, 0);	
+			} else if(reachedLower == false) {
+				transform.Translate(-HorizontalIncrement, 0, 0);	
+			}
+		}
+		
+		
+		//Up and Down floating platform
+		if((upperBound.y - lowerBound.y) != 0) {
+			if(this.transform.position.y >= upperBound.y) {
+					reachedUpper = true;
+					reachedLower = false;
+			}
+			if(this.transform.position.y <= lowerBound.y) {
+					reachedLower = true;
+					reachedUpper = false;
+			}
+			if(reachedUpper == false) {
+				transform.Translate(0, VerticalIncrement, 0);	
+			} else if(reachedLower == false) {
+				transform.Translate(0, -VerticalIncrement, 0);	
+			}
+		}
+	}
+	
+	
+	
+	
+}
