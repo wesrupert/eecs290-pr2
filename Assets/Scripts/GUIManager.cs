ae7ef7a2b2cbc96e1b6efc1a54eb759ecd6610c0
Wes Rupert <ora@outlook.com>
2013-02-19 20:01:54 -0500

    Added rudimentary GUI

--- Assets/Scripts/GUIManager.cs
+++ Assets/Scripts/GUIManager.cs
@@ -2,32 +2,29 @@ using UnityEngine;
 using System.Collections;
 
 public class GUIManager : MonoBehaviour {
-    private const string DEFAULTLEVEL = "New level";
-    private const string DEFAULTPLAYER = "Player 1"
-    private const int DEFAULTLIVES = 3;
+    private const string D_LEVEL = "New Level";
+    private const string D_PLAYER = "Player 1";
+    private const int D_SCORE = 0;
+    private const int D_LIVES = 3;
+    
+    private const string F_LEVELTEXT = "{0} - {1}";
+    private const string F_SCORE = "{0} lives - {1}";
 
-    public string level;
-    public int lives;
-    public float score;
+    public string level = D_LEVEL;
+    public string player = D_PLAYER;
+    public int score = D_SCORE;
+    public int lives = D_LIVES;
+
+    public GUIText levelText;
+    public GUIText scoreText;
 
 	// Use this for initialization
 	void Start () {
-        if (level == null || level == string.Empty) {
-            level = DEFAULTLEVEL;
-        }
-
-        if (lives == 0) {
-            lives = DEFAULTLIVES;
-        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        levelText.text = string.Format(F_LEVELTEXT, player, level);
+        scoreText.text = string.Format(F_SCORE, lives, score);
 	}
-
-    void OnGUI() {
-        GUI.Box(new Rect(0, 0, 150, 25), "Player 1 - " + level);
-        GUI.Box(new Rect(Screen.width - 150, 0, 150, 25), string.Format("{0} lives - {1}", lives, score));
-    }
 }
